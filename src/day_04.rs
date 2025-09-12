e57961a9beeac7b7a443e3d7639cfa8fb6d17822
MizardX <mizardx@gmail.com>
2025-09-12 20:54:30 +0200

    Day 04

---
+++ src/day_04.rs
@@ -0,0 +1,47 @@
+#[aoc(day4, part1)]
+fn solve_part_1(input: &[u8]) -> u32 {
+    let mut root = md5::Context::new();
+    root.consume(input.trim_ascii());
+    for x in 1_u32.. {
+        let mut ctx = root.clone();
+        ctx.consume(format!("{x}"));
+        let hash = ctx.finalize().0;
+        if matches!(hash, [0, 0, b, ..] if b >> 4 == 0) {
+            return x;
+        }
+    }
+    0
+}
+
+#[aoc(day4, part2)]
+fn solve_part_2(input: &[u8]) -> u32 {
+    let mut root = md5::Context::new();
+    root.consume(input.trim_ascii());
+    for x in 1_u32.. {
+        let mut ctx = root.clone();
+        ctx.consume(format!("{x}"));
+        let hash = ctx.finalize().0;
+        if matches!(hash, [0, 0, 0, ..]) {
+            return x;
+        }
+    }
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case(b"abcdef" => 609043)]
+    #[test_case(b"pqrstuv" => 1048970)]
+    fn test_part_1(input: &[u8]) -> u32 {
+        solve_part_1(input)
+    }
+
+    #[test_case(b"abcdef" => 6742839)]
+    #[test_case(b"pqrstuv" => 5714438)]
+    fn test_part_2(input: &[u8]) -> u32 {
+        solve_part_2(input)
+    }
+}
